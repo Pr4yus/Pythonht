d634f2ddbc1cd25a8de603c88f53868d381ca1ee
Abner <palacios.morales78@gmail.com>
2021-02-11 15:10:04 -0600

    Initial commit

---
+++ ht1.2.py
@@ -0,0 +1,6 @@
+print("Ingrese un numero")
+x = int(input())
+if x % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
---
+++ ht1.py
@@ -0,0 +1,7 @@
+print("Introduce un numero entero")
+x = int(input())
+for i in range(x):
+    for j in range(i+1):
+        print("*", end=" ")
+    print("")
+
